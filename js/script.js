e6d2c6d07a70add2117d83bdff089fe05726d751
Cristian <97061228+Eggward12@users.noreply.github.com>
2023-02-04 22:24:05 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,43 @@
+const togglemenu = document.querySelector(".toggle");
+const enlaces = document.querySelector(".responsivemenu");
+
+togglemenu.addEventListener('click',()=>{
+    enlaces.classList.toggle('activado');
+})
+
+const grandel = document.querySelector(".grande");
+const puntol = document.querySelectorAll(".punto");
+
+puntol.forEach((cadaPunto,i)=>{
+    puntol[i].addEventListener("click",()=>{
+        let posicion = i;
+
+        puntol.forEach((puntos,i)=>{
+            puntol[i].classList.remove("activo");//revisar
+        })
+
+        // puntol[0].classList.remove("activo");
+
+        switch (posicion) {
+            case 0:
+                grandel.style.transform = "translateX(0%)";
+
+                puntol[i].classList.add("activo");
+                break;
+        
+            case 1:
+                grandel.style.transform = "translateX(-33.3%)";
+                puntol[i].classList.add("activo");
+                break;
+            case 2:
+                grandel.style.transform = "translateX(-66.6%)";
+                puntol[i].classList.add("activo");
+                
+                break;
+            default:
+                
+                break;
+        }
+    })
+
+});
